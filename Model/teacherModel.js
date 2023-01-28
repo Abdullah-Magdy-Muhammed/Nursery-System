65bc7800c338e6f7caf5c95cc0ea107d41ec4aaa
Abdullah <56775516+Zaazou@users.noreply.github.com>
2023-01-28 21:53:52 +0200

    hasing passwords

--- Model/teacherModel.js
+++ Model/teacherModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require('bcrypt')
 // to create schema of techer
 const schema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
@@ -10,6 +10,11 @@ const schema = new mongoose.Schema({
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address']
     },
     image: { type: String }
+});
+schema.pre('save', async function (next) {
+    const salt = await bcrypt.genSalt();
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
 })
 
 mongoose.model("teachers", schema)
